a2e68d0fa2699ef6f7c91fa3666aee0e68547317
LucasLevingston <lucaolevingston@outlook.com>
2024-04-02 21:54:48 -0300

    Modificadas rotas para usuarios nao logados.

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,52 @@ import CadastroFuncionario1 from './pages/cadastro/Funcionario/CadastroFuncionar
 import CadastroUsuario from './pages/cadastro/Usuario/CadastroUsuario';
 import RedefinirSenha from './pages/cadastro/Usuario/RedefinirSenha';
 import GerarPDF from './components/PDF/GerarPDF';
+import { useAuntenticacao } from './hooks/usuarios.hooks';
+import { ReloadIcon } from '@radix-ui/react-icons';
 
 export default function App() {
+	const { carregando, usuario } = useAuntenticacao();
+
 	return (
 		<BrowserRouter>
+			{carregando ? (
+				<div className="flex h-full w-full flex-col items-center justify-center space-y-5">
+					<div>Carregando...</div>
+					<ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+				</div>
+			) : usuario ? (
+				<Routes>
+					<Route path={`/get-funcionario/:id`} element={<GetFuncionario />} />
+					<Route
+						path="/cadastro-funcionario"
+						element={<CadastroFuncionario1 />}
+					/>
+					<Route path="/get-funcionarios" element={<GetTodosFuncionarios />} />
+					<Route path="/gerar-pdf/:id" element={<GerarPDF />} />
+					<Route
+						path="/get-funcionario"
+						element={<Navigate to="/get-funcionarios" />}
+					/>
+				</Routes>
+			) : (
+				<Routes>
+					<Route
+						path={`/get-funcionario/:id`}
+						element={<Navigate to="/login" />}
+					/>
+					<Route
+						path="/cadastro-funcionario"
+						element={<Navigate to="/login" />}
+					/>
+					<Route path="/get-funcionarios" element={<Navigate to="/login" />} />
+					<Route path="/gerar-pdf/:id" element={<Navigate to="/login" />} />
+					<Route path="/get-funcionario" element={<Navigate to="/login" />} />
+				</Routes>
+			)}
 			<Routes>
-				<Route path={`/get-funcionario/:id`} element={<GetFuncionario />} />
 				<Route path="/cadastro-usuario" element={<CadastroUsuario />} />
-
 				<Route path="/redefinir-senha" element={<RedefinirSenha />} />
-				<Route
-					path="/cadastro-funcionario"
-					element={<CadastroFuncionario1 />}
-				/>
-				<Route path="/get-funcionarios" element={<GetTodosFuncionarios />} />
-				<Route path="/gerar-pdf/:id" element={<GerarPDF />} />
-				<Route
-					path="/get-funcionario"
-					element={<Navigate to="/get-funcionarios" />}
-				/>
-				<Route path="/login" element={<Login />} caseSensitive={false} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/" element={<Navigate to="/get-funcionarios" />} />
 			</Routes>
 		</BrowserRouter>
