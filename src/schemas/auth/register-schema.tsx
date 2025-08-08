e1a60a078dcfb944cc30c228a578d92b036bd036
LucasLevingston <lucaolevingston@outlook.com>
2025-08-08 15:01:11 -0300

    added schemas

---
+++ src/schemas/auth/register-schema.tsx
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+export const registerSchema = z
+  .object({
+    email: z.string().email({ message: 'Email inválido' }),
+    cpf: z.string(),
+    name: z.string(),
+    password: z
+      .string()
+      .min(6, { message: 'A senha deve ter no mínimo 6 caracteres' }),
+    confirmPassword: z.string().min(6, {
+      message: 'A confirmação de senha deve ter no mínimo 6 caracteres',
+    }),
+  })
+  .refine(data => data.password === data.confirmPassword, {
+    message: 'As senhas não coincidem',
+    path: ['confirmPassword'],
+  })
