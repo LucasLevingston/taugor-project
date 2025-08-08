dd0ac253b939d735078b13d8cbff96b0c7214a3c
LucasLevingston <lucaolevingston@outlook.com>
2025-08-08 14:57:26 -0300

    no schemas

--- src/schemas/auth/login-schema.ts.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-
-export const loginSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
-  password: z
-    .string()
-    .min(8, { message: 'Password must be at least 8 characters long' }),
-})
