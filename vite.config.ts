ba452e6f8beea7a881635404fa75a00a7f5678b3
LucasLevingston <lucaolevingston@outlook.com>
2025-08-08 14:40:06 -0300

    fix: add tsconfig paths support to vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 
   resolve: {
     alias: {
